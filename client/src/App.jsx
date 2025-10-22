ce4bcf24f873460b9e029145b31162f784e0f169
waibaBikash <kavrelibikash@gmail.com>
2025-10-22 22:21:27 +1100

    Refactor App component to include routing and main page structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
 
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+import MovieDetails from './pages/MovieDetails'
+import SearLayout from './pages/SearLayout'
+import MyBookihgs from './pages/MyBookihgs'
+import Favorite from './pages/Favorite'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+       <Routes >
+          <Route  path='/' element={<Home/>}/>
+          <Route  path='/movies' element={<Movies/>}/>
+          <Route  path='/movies/:id' element={<MovieDetails/>}/>
+          <Route  path='/movies/:id/:date' element={<SearLayout/>}/>
+          <Route  path='/my-bookings' element={<MyBookihgs/>}/>
+          <Route  path='favorite' element={<Favorite/>}/>
+       </Routes>
+    </>
   )
 }
 
